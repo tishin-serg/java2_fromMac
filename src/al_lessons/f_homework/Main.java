ff395c864f620fcd994ae3398caccfa1d9be3300
tishin-serg <tishinsergey4@gmail.com>
2021-04-27 18:04:40 +0300

    Задание для пятого вебинара

--- src/al_lessons/f_homework/Main.java
+++ src/al_lessons/f_homework/Main.java
@@ -2,12 +2,14 @@ package al_lessons.f_homework;
 
 public class Main {
 
-    public static void main(String[] args) {
-        firstMethod();
-        secondMethod();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println(checkArray(firstMethod(), secondMethod()));
+
     }
 
-    public static void firstMethod() {
+    public static float[] firstMethod() {
 
         final int size = 10000000;
         float[] arr = new float[size];
@@ -24,9 +26,11 @@ public class Main {
 
         System.out.println(System.currentTimeMillis() - a);
 
+        return arr;
+
     }
 
-    public static void secondMethod() {
+    public static float[] secondMethod() throws InterruptedException {
         final int size = 10000000;
         final int h = size / 2;
         float[] arr = new float[size];
@@ -43,32 +47,48 @@ public class Main {
         System.arraycopy(arr, 0, array1, 0, h);
         System.arraycopy(arr, h, array2, 0, h);
 
+
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
+                System.out.println("Thread1 started");
                 for (int i = 0; i < array1.length; i++) {
-                    arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                    array1[i] = (float) (array1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
+                System.out.println("Thread1 finished");
             }
         });
 
         Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
+                System.out.println("Thread2 started");
                 for (int i = 0; i < array2.length; i++) {
-                    arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                    array2[i] =
+                            (float) (array2[i] * Math.sin(0.2f + (i + h)/ 5) * Math.cos(0.2f + (i + h)/ 5) * Math.cos(0.4f + (i + h) / 2));
                 }
+                System.out.println("Thread2 finished");
             }
         });
 
         thread1.start();
         thread2.start();
+        thread1.join();
+        thread2.join();
 
         System.arraycopy(array1, 0, arr, 0, h);
         System.arraycopy(array2, 0, arr, h, h);
 
         System.out.println(System.currentTimeMillis() - a);
 
+        return arr;
+    }
 
+    public static boolean checkArray(float[] arr1, float[] arr2) {
+        for (int i = 0; i < arr1.length; i++) {
+            if(arr1[i] != arr2[i])
+                return false;
+        }
+        return true;
     }
 }
